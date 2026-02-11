578e082933aa37a91853b47df79c4441e76b0bd8
Praneetha2017 <2400032017@kluniversity.in>
2026-02-11 14:42:42 +0530

    11-feb

---
+++ SpringBootDemoApp/src/main/java/com/klu/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.klu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class DemoController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "This is springboot application";
+		
+	}
+	@GetMapping("/bye")
+	public String sayBye() {
+		return "Have a good day!";
+	}
+
+}
